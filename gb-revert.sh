824522950b57a693ee550b8fe740f77e81ec346c
Amadeusz Leonardo Juskowiak <amadeusz.leonardo@gmail.com>
2012-04-17 20:38:34 +0200

    forgot to add reverting

---
+++ gb-revert.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+DIR=`pwd`
+
+GIT=/usr/bin/git
+GB="`dirname $0`/gb.sh"
+
+if [ ! -e "$DIR/.git" ]; then
+  echo "gb-revert: not in git repository."
+  exit 1
+else
+  if [ ! -n "$1" ]; then
+    echo "gb-revert: add filename you want to revert."
+    exit 1
+  else
+    git log --stat --pretty="format:%h on %s, changes: " $1
+
+    file_rev='qwerrtyu'
+    until git branch -a --contains $file_rev &> /dev/null ; do
+      echo -n "Which revision? "
+      read file_rev
+    done
+   
+    cp -r $1 "$1.current_backup"
+    echo "gb-revert: made backup to $1.current_backup"
+
+    git checkout $file_rev $1
+    echo "gb-revert: file $1 reverted."
+  fi
+fi
