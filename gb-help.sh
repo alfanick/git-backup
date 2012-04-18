222671635f592a17c0a08f3ff5eb0ae532b46a6b
Amadeusz Leonardo Juskowiak <amadeusz.leonardo@gmail.com>
2012-04-18 15:30:28 +0200

    Added help

---
+++ gb-help.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "Git Backup Help:"
+echo "\tgb create - create Git repository on local machine and server and connects them"
+echo "\tgb backup - creates a snapshot and send to server"
+echo "\tgb sync - send repository to server"
+echo "\tgb revert filename - shows log of the file and allows to revert it (while creating backup of current version)"
+echo "\tgb find text - looks for text in a repository"
+echo "\tgb help - shows this help"
